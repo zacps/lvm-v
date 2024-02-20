c7c4910722e509ee4f737fd1d77b4a2f0907f034
Zac Pullar-Strecker <zacmps@gmail.com>
2024-02-20 22:15:03 +1300

    Run real commands, format with ruff

--- lvm_v/main.py
+++ lvm_v/main.py
@@ -1,20 +1,23 @@
 import json
+import re
 import subprocess
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-
 def run_lvm_command(command):
-    # output = subprocess.run([
-    #     command,
-    #     "--reportformat",
-    #     "json"
-    # ], stdout=subprocess.PIPE).stdout
+    output = subprocess.run(
+        [command, "--reportformat", "json"], stdout=subprocess.PIPE
+    ).stdout
 
-    with open(f"lvm_v/{command}.json") as f:
-        output = json.load(f)
+    data = json.loads(output)["report"][0][command[:-1]]
 
-    data = output["report"][0]
+    return data
+
+
+def run_lsblk():
+    output = subprocess.run(["lsblk", "J"], stdout=subprocess.PIPE).stdout
+
+    data = json.loads(output)["blockdevices"]
 
     return data
 
@@ -31,12 +34,12 @@ def find_mountpoint(lsblk, name):
 
     return find_mountpoint(children, name)
 
+
 def main():
-    lvs = run_lvm_command("lvs")["lv"]
-    pvs = run_lvm_command("pvs")["pv"]
-    vgs = run_lvm_command("vgs")["vg"]
-    with open("lvm_v/lsblk.json") as f:
-        lsblk = json.load(f)["blockdevices"]
+    lvs = run_lvm_command("lvs")
+    pvs = run_lvm_command("pvs")
+    vgs = run_lvm_command("vgs")
+    lsblk = run_lsblk()
 
     thins, lvs_no_thin = [], []
     for vol in lvs:
@@ -44,6 +47,7 @@ def main():
             thins.append(vol)
         else:
             lvs_no_thin.append(vol)
+            # Add fake thin pool for layout
             if not vol["pool_lv"]:
                 thins.append({"fake": True})
     lvs = lvs_no_thin
@@ -51,8 +55,6 @@ def main():
     # Assign mount points to logical volumes from lsblk
     for vol in lvs:
         vol["mountpoint"] = find_mountpoint(lsblk, f'{vol["vg_name"]}-{vol["lv_name"]}')
-        if vol["mountpoint"] is None:
-            print("Failed to find mountpoint for ", f'{vol["vg_name"]}-{vol["lv_name"]}')
 
     # Add disks and partitions from lsblk
     disks, partitions = [], []
@@ -63,42 +65,59 @@ def main():
         for child in disk["children"]:
             if child["type"] == "part":
                 part_name = f'/dev/{child["name"]}'
-                partitions.append({
-                    "name": part_name,
-                    "size": child["size"].lower(),
-                    "lvm_allocated": part_name in {pv["pv_name"] for pv in pvs},
-                    "disk": name,
-                    "fake": False,
-                    "mountpoint": child["mountpoint"]
-                })
+                partitions.append(
+                    {
+                        "name": part_name,
+                        "size": child["size"].lower(),
+                        "lvm_allocated": part_name in {pv["pv_name"] for pv in pvs},
+                        "disk": name,
+                        "fake": False,
+                        "mountpoint": child["mountpoint"],
+                    }
+                )
                 parts += 1
 
         lvm_allocated = name in {pv["pv_name"] for pv in pvs}
-        disks.append({
-            "name": name,
-            "size": disk["size"].lower(),
-            "lvm_allocated": lvm_allocated,
-            "parts": parts
-        })
+        disks.append(
+            {
+                "name": name,
+                "size": disk["size"].lower(),
+                "lvm_allocated": lvm_allocated,
+                "parts": parts,
+            }
+        )
+        # Add fake partitions for layout if lvm is using the whole disk
         if lvm_allocated:
-            partitions.append({
-                "name": f"{name}_fakepartition",
-                "size": "",
-                "lvm_allocated": False,
-                "disk": name,
-                "fake": True
-            })
+            partitions.append(
+                {
+                    "name": f"{name}_fakepartition",
+                    "size": "",
+                    "lvm_allocated": False,
+                    "disk": name,
+                    "fake": True,
+                }
+            )
 
     env = Environment(
-        loader=FileSystemLoader("lvm_v/templates"),
-        autoescape=select_autoescape()
+        loader=FileSystemLoader("lvm_v/templates"), autoescape=select_autoescape()
     )
 
     template = env.get_template("lvm.mermaid")
 
-    print("---")
+    rendered = template.render(
+        lvs=lvs,
+        pvs=pvs,
+        vgs=vgs,
+        disks=disks,
+        partitions=partitions,
+        thins=thins,
+        render_mountpoints=True,
+    )
+    # FIXME: This doesn't work for some reason
+    formatted = re.sub("\n{2,}", "\n\n", rendered)
 
-    print(template.render(lvs=lvs, pvs=pvs, vgs=vgs, disks=disks, partitions=partitions, thins=thins))
+    print(formatted)
+
 
 if __name__ == "__main__":
     main()
