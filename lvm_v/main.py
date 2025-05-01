d489c0cca9b9929bc75cc60e983abf9a4e25e61f
Zac Pullar-Strecker <zacmps@gmail.com>
2025-05-01 17:55:21 +1200

    LVM-v 0.2

--- lvm_v/main.py
+++ lvm_v/main.py
@@ -4,6 +4,9 @@ import subprocess
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
+COLOR_CYCLE = ['80b1d3', 'fb8072', 'bc80bd', 'b3de69','fdb462','fccde5','8dd3c7','ffed6f','bebada', 'ccebc5', 'd9d9d9']
+
+
 def run_lvm_command(command):
     output = subprocess.run(
         [command, "--reportformat", "json"], stdout=subprocess.PIPE
@@ -35,12 +38,23 @@ def find_mountpoint(lsblk, name):
     return find_mountpoint(children, name)
 
 
-def main():
-    lvs = run_lvm_command("lvs")
-    pvs = run_lvm_command("pvs")
-    vgs = run_lvm_command("vgs")
-    lsblk = run_lsblk()
+def generate_diagram(pvs, vgs, lvs, lsblk):
+    # TODO: Stable sort everything by volume group association. This should mean nicer diagrams in most cases
+    # the ugly case is when a drive has multiple partitions in different VGs.
 
+    # Assign colors to VGs:
+    for i, group in enumerate(vgs):
+        group["color"] = COLOR_CYCLE[i]
+
+    # Assign colors to LVs (don't discriminate between thin/regular)
+    for vol in lvs:
+        vol["color"] = next(group["color"] for group in vgs if group["vg_name"] == vol["vg_name"])
+
+    # Assign colorsr to PVs
+    for vol in pvs:
+        vol["color"] = next(group["color"] for group in vgs if group["vg_name"] == vol["vg_name"])
+
+    # Divide LVs into thin pools and regular volumes
     thins, lvs_no_thin = [], []
     for vol in lvs:
         if vol["lv_attr"][0] == "t":
@@ -58,7 +72,7 @@ def main():
 
     # Add disks and partitions from lsblk
     disks, partitions = [], []
-    for disk in lsblk:
+    for i, disk in enumerate(lsblk):
         name = f'/dev/{disk["name"]}'
 
         parts = 0
@@ -75,6 +89,13 @@ def main():
                         "mountpoint": child["mountpoint"],
                     }
                 )
+                # Potentially add a fake physical volume if it's unallocated
+                if not partitions[-1]["lvm_allocated"]:
+                    pvs.insert(i+1, {
+                        "fake": True,
+                        "pv_name": "",
+                        "pv_size": ""
+                    })
                 parts += 1
 
         lvm_allocated = name in {pv["pv_name"] for pv in pvs}
@@ -104,7 +125,7 @@ def main():
 
     template = env.get_template("lvm.mermaid")
 
-    rendered = template.render(
+    return template.render(
         lvs=lvs,
         pvs=pvs,
         vgs=vgs,
@@ -113,6 +134,16 @@ def main():
         thins=thins,
         render_mountpoints=True,
     )
+
+
+def main():
+    lvs = run_lvm_command("lvs")
+    pvs = run_lvm_command("pvs")
+    vgs = run_lvm_command("vgs")
+    lsblk = run_lsblk()
+
+    rendered = generate_diagram(pvs, vgs, lvs, lsblk)
+
     # FIXME: This doesn't work for some reason
     formatted = re.sub("\n{2,}", "\n\n", rendered)
 
